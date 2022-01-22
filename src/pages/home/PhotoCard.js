a77b067179b53e5e10fbc7f501027fa2a4aaf9f0
sanath <sanath.yapatla@prasthana.com>
2022-01-22 12:21:26 +0530

    remove warnings

--- src/pages/home/PhotoCard.js
+++ src/pages/home/PhotoCard.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { useEffect } from "react/cjs/react.development";
 
 const StyledPhotoCard = styled.img`
   object-fit: cover;
